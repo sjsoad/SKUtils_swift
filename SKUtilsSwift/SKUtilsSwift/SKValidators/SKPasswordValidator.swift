befba16df2ef4de4ac214454263cbc380a1ef98b
sjsoad <sjsoad666@yandex.ru>
2016-07-17 10:27:24 +0300

    SKPasswordValidator added tests added

---
+++ SKUtilsSwift/SKUtilsSwift/SKValidators/SKPasswordValidator.swift
@@ -0,0 +1,13 @@
+//
+//  SKPasswordValidator.swift
+//  SKUtilsSwift
+//
+//  Created by Sergey Kostyan on 17.07.16.
+//  Copyright © 2016 Sergey Kostyan. All rights reserved.
+//
+
+import UIKit
+
+class SKPasswordValidator: SKBaseValidator {
+
+}
