fd87c2735621244186aaba20b8666aeeac958ebe
Mac <skostian@grossum.com>
2016-07-20 18:52:59 +0300

    no message

--- SKUtilsSwift/Pods/Target Support Files/PMTween/PMTween-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PMTween : NSObject
-@end
-@implementation PodsDummy_PMTween
-@end
--- SKUtilsSwift/Pods/Target Support Files/Pods-SKUtilsSwift/Pods-SKUtilsSwift-frameworks.sh
+++ SKUtilsSwift/Pods/Target Support Files/Pods-SKUtilsSwift/Pods-SKUtilsSwift-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/PMTween/PMTween.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Spring/Spring.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/PMTween/PMTween.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Spring/Spring.framework"
 fi
