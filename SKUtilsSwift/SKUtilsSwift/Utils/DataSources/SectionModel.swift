0ca27b950818f81010c5d46bcb788cd8f0d2c39c
sjsoad <sjsoad666@yandex.ru>
2016-09-10 13:31:37 +0300

    SectionModel refactoring

--- SKUtilsSwift/SKUtilsSwift/Utils/DataSources/SectionModel.swift
+++ SKUtilsSwift/SKUtilsSwift/Utils/DataSources/SectionModel.swift
@@ -13,12 +13,36 @@ class SectionModel: NSObject {
     var items: Array<AnyObject> = []
     var header: SectionHeader?
     var footer: SectionFooter?
+    
+    init(withItems items: Array<AnyObject>,
+                   header: SectionHeader,
+                   footer: SectionFooter) {
+        self.items = items
+        self.header = header
+        self.footer = footer
+    }
 }
 
 class SectionHeader: NSObject {
+    
     var headerTitle: String?
+    
+    class func header(withTitle title:String?) -> SectionHeader {
+        let header = SectionHeader()
+        header.headerTitle = title
+        return header
+    }
+    
 }
 
 class SectionFooter: NSObject {
+    
     var footerTitle: String?
+    
+    class func footer(withTitle title:String?) -> SectionFooter {
+        let footer = SectionFooter()
+        footer.footerTitle = title
+        return footer
+    }
+
 }
