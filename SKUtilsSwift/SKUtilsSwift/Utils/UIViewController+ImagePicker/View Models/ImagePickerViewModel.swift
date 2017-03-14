3e560fbee8a4832d23b171e69c7be5c09dbb290e
Sergey <sjsoad666@yandex.ru>
2017-03-14 15:36:25 +0200

    image picker added

---
+++ SKUtilsSwift/SKUtilsSwift/Utils/UIViewController+ImagePicker/View Models/ImagePickerViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  IamgePickerViewModel.swift
+//  GeoTouch
+//
+//  Created by Sergey on 06.03.17.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerViewModel: NSObject {
+
+    var selectedImage: Dynamic<UIImage?> = Dynamic(nil)
+    
+}
