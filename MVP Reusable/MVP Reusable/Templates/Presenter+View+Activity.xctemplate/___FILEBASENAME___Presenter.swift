2836829e43fb8fdcd91a9ae0d31d909c3e61c3c1
Sergey <sjsoad666@yandex.ru>
2017-05-17 14:22:37 +0300

    no message

---
+++ MVP Reusable/MVP Reusable/Templates/Presenter+View+Activity.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___Interface: class, ActivityProtocol {
+    
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___Output: RequestExecutingProtocol {
+    
+}
+
+class ___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___Output {
+    
+    private weak var view: ___FILEBASENAMEASIDENTIFIER___Interface?
+    
+    init(with view: ___FILEBASENAMEASIDENTIFIER___Interface) {
+        self.view = view
+    }
+    
+    //MARK -
+    
+    static func showView(in <#window,navigation,etc#>: <#type#>) {
+        let vc = ___FILEBASENAMEASIDENTIFIER___ViewController()
+        let presenter = ___FILEBASENAMEASIDENTIFIER___Presenter(with: vc)
+        vc.presenter = presenter
+        //navigation
+    }
+    
+    //MARK: - Activity Protocol
+    
+    func activityView() -> ActivityProtocol? {
+        return view
+    }
+    
+}
