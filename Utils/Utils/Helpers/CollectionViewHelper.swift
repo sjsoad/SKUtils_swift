90eb7ad51bd53a228fe512b34ba8a8ebebb9db09
Sergey <sjsoad666@yandex.ru>
2017-06-01 14:54:33 +0300

    collection view cell size helper

---
+++ Utils/Utils/Helpers/CollectionViewHelper.swift
@@ -0,0 +1,27 @@
+//
+//  CollectionViewHelper.swift
+//  Nioxin
+//
+//  Created by Sergey on 31.05.17.
+//  Copyright © 2017 grossum solutions. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewHelper: NSObject {
+
+    static func cellSize(for collectionView: UICollectionView,
+                         layout collectionViewLayout: UICollectionViewLayout,
+                         numberOfCellsInRow: CGFloat,
+                         cellHeightRation: CGFloat) -> CGSize {
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return CGSize.zero }
+        let insetsSpacing = flowLayout.sectionInset.left + flowLayout.sectionInset.right
+        let spacingBetweenCells = (numberOfCellsInRow - 1) * flowLayout.minimumInteritemSpacing
+        let content = collectionView.bounds.width - insetsSpacing - spacingBetweenCells
+        let width = content / numberOfCellsInRow
+        let height = width * cellHeightRation
+        let cellSize = CGSize(width: width, height: height)
+        return cellSize
+    }
+    
+}
