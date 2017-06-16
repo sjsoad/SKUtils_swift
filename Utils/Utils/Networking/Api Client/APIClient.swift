70ab0f670e3c67e1dfc521e0b899a9adad9db874
Sergey <sjsoad666@yandex.ru>
2017-06-16 17:12:12 +0300

    no message

--- Utils/Utils/Networking/Api Client/APIClient.swift
+++ Utils/Utils/Networking/Api Client/APIClient.swift
@@ -6,14 +6,17 @@
 //  Copyright © 2016 Sergey Kostyan. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import Alamofire
 
 typealias ErrorHandler = (_ error: Error) -> Void
 
 class APIClient: NSObject {
-
+    
     private var sessionManager: SessionManager!
+    private(set) var reachabilityManager: NetworkReachabilityManager?
+    private(set) var host: String?
+    private var reahcabilityErrorMessage: String = "Network connection seems to be offline"
     
     // MARK: - Init
     
@@ -21,30 +24,26 @@ class APIClient: NSObject {
         self.sessionManager = sessionManager
     }
     
-    // MARK: - Public Methods -
+    // MARK: - Utils -
+    
+    func reachabilityManagerWith(host: String, reachabilityError message: String) -> Bool {
+        guard let reachabilityManager = NetworkReachabilityManager(host: host) else { return false }
+        reachabilityManager.listener = { status in
+            print("Network Status Changed: \(status)")
+        }
+        reachabilityManager.startListening()
+        self.reachabilityManager = reachabilityManager
+        self.host = host
+        self.reahcabilityErrorMessage = message
+        return true
+    }
+    
+    // MARK: - Public Methods
     
     func executeRequest<T: APIRequestProtocol>(request: T,
-                        success: ((_ response: T.Response) -> Void)? = nil,
-                        failure: ErrorHandler? = nil) -> Request? {
-        return sessionManager.request(request.path,
-                                      method: request.HTTPMethod,
-                                      parameters: request.parameters,
-                                      encoding: JSONEncoding.default,
-                                      headers: request.headers)
-            .responseJSON(completionHandler: { (response) in
-                switch response.result {
-                case .success:
-                    if let successClosure = success,
-                        let json = response.result.value {
-                        let response: T.Response = T.Response(JSON: json as AnyObject)
-                        successClosure(response)
-                    }
-                case .failure(let error):
-                    if let errorClosure = failure {
-                        errorClosure(error)
-                    }
-                }
-            })
+                                               success: ((_ response: T.Response) -> Void)? = nil,
+                                               failure: ErrorHandler? = nil) -> Request? {
+        return execute(request: request, success: success, failure: failure)
     }
     
     func pauseAllRequests(pause: Bool) {
@@ -59,6 +58,40 @@ class APIClient: NSObject {
         if let taskToCancel = task {
             taskToCancel.cancel()
         }
+    }
+    
+    // MARK: - Private -
+    
+    private func networkConnectionError(host: String) -> Error {
+        return NSError(domain: host, code: 404, userInfo: [NSLocalizedDescriptionKey: reahcabilityErrorMessage])
+    }
+    
+    private func execute<T: APIRequestProtocol>(request: T,
+                                                success: ((_ response: T.Response) -> Void)? = nil,
+                                                failure: ErrorHandler? = nil) -> Request? {
+        return sessionManager.request(request.path,
+                                      method: request.HTTPMethod,
+                                      parameters: request.parameters,
+                                      encoding: JSONEncoding.default,
+                                      headers: request.headers)
+            .responseJSON(completionHandler: { (response) in
+                switch response.result {
+                case .success:
+                    if let successClosure = success,
+                        let json = response.result.value {
+                        if let error = NetworkErrorHelper.errorFrom(JSON: json), let errorClosure = failure {
+                            errorClosure(error)
+                        } else {
+                            let response: T.Response = T.Response(JSON: json as AnyObject)
+                            successClosure(response)
+                        }
+                    }
+                case .failure(let error):
+                    if let errorClosure = failure {
+                        errorClosure(error)
+                    }
+                }
+            })
     }
     
 }
