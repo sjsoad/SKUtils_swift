baf3a997eab00e18aa740b1a0733e3b9e91467ef
sjsoad <sjsoad666@yandex.ru>
2017-06-17 15:28:01 +0300

    added date text field component

---
+++ Utils/Utils/TextFields/DateTextField.swift
@@ -0,0 +1,21 @@
+//
+//  DateTextField.swift
+//  SwiftUtils
+//
+//  Created by Sergey Kostyan on 17.06.17.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class DateTextField: BaseTextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
