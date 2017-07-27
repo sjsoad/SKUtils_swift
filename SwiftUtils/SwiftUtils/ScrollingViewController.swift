0e6aef9ccb5e56feeaacad34b36c6c3afab0c7cd
Sergey <sjsoad666@yandex.ru>
2017-07-27 15:21:19 +0300

    button sticked to bottom or else

---
+++ SwiftUtils/SwiftUtils/ScrollingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScrollingViewController.swift
+//  SwiftUtils
+//
+//  Created by Sergey on 27.07.17.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ScrollingViewController: UIViewController {
+
+    @IBOutlet private weak var textInputsManager: TextInputsManager!
+    
+}
