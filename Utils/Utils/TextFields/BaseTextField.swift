f68f22bdd390c3a999444ddfa1a18e5c44a9b9bf
Sergey <sjsoad666@yandex.ru>
2017-07-31 13:51:50 +0300

    added placeholder color as property of BaseTextField

--- Utils/Utils/TextFields/BaseTextField.swift
+++ Utils/Utils/TextFields/BaseTextField.swift
@@ -10,6 +10,24 @@ import UIKit
 
 public class BaseTextField: UITextField {
     
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            self.updateAttributtedPlaceholder()
+        }
+    }
+    
     @IBOutlet var visualisationView: TextFieldAccessoryView?
 
+    public override var placeholder: String? {
+        didSet {
+            self.updateAttributtedPlaceholder()
+        }
+    }
+    
+    private func updateAttributtedPlaceholder() {
+        guard let text = self.placeholder, let color = self.placeholderColor else { return }
+        self.attributedPlaceholder = NSAttributedString(string: text,
+                                                        attributes:[NSForegroundColorAttributeName: color])
+    }
+    
 }
