091e6e9cf70dd2b57da86a386bb4014509d81c27
Sergey <sjsoad666@yandex.ru>
2018-03-12 09:51:21 +0200

    added protocols for image picker

---
+++ Utils/Utils/Protocols/MVP/Presenter/ImagePickingOutput.swift
@@ -0,0 +1,94 @@
+//
+//  ImagePickingOutput.swift
+//  Yada
+//
+//  Created by Sergey on 07.03.2018.
+//Copyright © 2018 Sergey. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+import Photos
+
+// Extend your Presenter with this protocol
+
+protocol ImagePicking: class {
+    
+    // make private(set)
+    var cameraPermissions: CameraPermissions? { get }
+    var photoLibraryPermissions: PhotoLibraryPermissions? { get }
+    
+    func imagePickingInterface() -> ImagePickingInterface?
+    func showImagePicker(with sourceType: UIImagePickerControllerSourceType)
+    
+}
+
+extension ImagePicking {
+    
+    func showImagePicker(with sourceType: UIImagePickerControllerSourceType) {
+        imagePickingInterface()?.showImagePicker(with: sourceType)
+    }
+    
+}
+
+// Extend your Output protocol with this protocol
+
+protocol ImagePickingOutput {
+
+    func viewTriggeredCallImagePickerEvent()
+    func viewTriggeredSelectionOfPicker(sourceType: UIImagePickerControllerSourceType)
+    func viewTriggedImageSelectionEvent(with image: UIImage)
+    
+}
+
+extension ImagePickingOutput where Self: ImagePicking {
+    
+    func viewTriggeredCallImagePickerEvent() {
+        imagePickingInterface()?.showImagePickerAlert()
+    }
+    
+    func viewTriggeredSelectionOfPicker(sourceType: UIImagePickerControllerSourceType) {
+        if sourceType == .camera {
+            checkCameraPermissions()
+        }
+        if sourceType == .photoLibrary {
+            checkCameraRollPermissions()
+        }
+    }
+    
+    // MARK: - Private -
+    
+    private func checkCameraPermissions() {
+        guard let cameraPermissions = cameraPermissions else { return }
+        let state: AVAuthorizationStatus = cameraPermissions.permissionsState()
+        if state == .notDetermined {
+            cameraPermissions.requestPermissions(handler: { [weak self] (_) in
+                self?.checkCameraPermissions()
+            })
+        }
+        if state == .authorized {
+            showImagePicker(with: .camera)
+        }
+        if state == .denied {
+            cameraPermissions.showSettingsAlert()
+        }
+    }
+    
+    private func checkCameraRollPermissions() {
+        guard let photoLibraryPermissions = photoLibraryPermissions else { return }
+        let state: PHAuthorizationStatus = photoLibraryPermissions.permissionsState()
+        if state == .notDetermined {
+            photoLibraryPermissions.requestPermissions(handler: { [weak self] (_) in
+                self?.checkCameraRollPermissions()
+            })
+        }
+        if state == .authorized {
+            showImagePicker(with: .photoLibrary)
+        }
+        if state == .denied {
+            photoLibraryPermissions.showSettingsAlert()
+        }
+    }
+    
+}
---
+++ Utils/Utils/Protocols/MVP/View/ImagePickingInterface.swift
@@ -0,0 +1,64 @@
+//
+//  ImagePickingInterface.swift
+//  Yada
+//
+//  Created by Sergey on 07.03.2018.
+//Copyright © 2018 Sergey. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+// Extend your UIViewController with this protocol
+
+protocol ImagePickerAlertConfigurating: UIImagePickerControllerDelegate, UINavigationControllerDelegate where Self: UIViewController {
+    
+    var imagePickerAlertTitle: String? { get }
+    var imagePickerAlertMessage: String? { get }
+    var imagePickerAlertCameraTitle: String? { get }
+    var imagePickerAlertLibraryTitle: String? { get }
+    var imagePickerAlertCancelTitle: String? { get }
+    var cameraSelectionHandler: (() -> Void)? { get }
+    var librarySelectionHandler: (() -> Void)? { get }
+    
+}
+
+extension ImagePickerAlertConfigurating {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+// Extend your Interface protocol with this protocol
+
+protocol ImagePickingInterface {
+
+    func showImagePickerAlert()
+    func showImagePicker(with sourceType: UIImagePickerControllerSourceType)
+    
+}
+
+extension ImagePickingInterface where Self: ImagePickerAlertConfigurating {
+
+    func showImagePickerAlert() {
+        let actionSheet = UIAlertController(title: imagePickerAlertTitle,
+                                            message: imagePickerAlertMessage,
+                                            preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction.defaultAction(title: imagePickerAlertCameraTitle, handler: { [weak self] (_) in
+            self?.cameraSelectionHandler?()
+        }))
+        actionSheet.addAction(UIAlertAction.defaultAction(title: imagePickerAlertLibraryTitle, handler: { [weak self] (_) in
+            self?.librarySelectionHandler?()
+        }))
+        actionSheet.addAction(UIAlertAction.cancelAction(title: imagePickerAlertCancelTitle))
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func showImagePicker(with sourceType: UIImagePickerControllerSourceType) {
+        guard let imagePicker = ImagePickerFactory.imagePicker(sourceType: sourceType, delegate: self, allowEdititng: true) else { return }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
