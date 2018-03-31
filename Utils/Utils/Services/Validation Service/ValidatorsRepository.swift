a90d1fb480e76d827ec1f16222afa40cd5da2427
sjsoad <sjsoad666@yandex.ru>
2018-03-31 11:25:53 +0300

    no message

---
+++ Utils/Utils/Services/Validation Service/ValidatorsRepository.swift
@@ -0,0 +1,30 @@
+//
+//  ValidatorsRepository.swift
+//  SwiftUtils
+//
+//  Created by Sergey on 01.12.2017.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class DefaultValidatorsRepository {
+
+    private var registry = [String: Any]()
+    
+    init() {}
+    
+    func registerService<T>(service: T) {
+        let key = "\(T.self)"
+        registry[key] = service
+    }
+    
+    func getService<T>() -> T? {
+        let key = "\(T.self)"
+        if registry[key] == nil {
+            print("ServicesRepository: Nil value for \(key)")
+        }
+        return registry[key] as? T
+    }
+    
+}
---
+++ Utils/Utils/Text Inputs/ToolbarTextView.swift
@@ -0,0 +1,21 @@
+//
+//  ToolbarTextView.swift
+//  SwiftUtils
+//
+//  Created by Sergey Kostyan on 31.03.2018.
+//  Copyright © 2018 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ToolbarTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
