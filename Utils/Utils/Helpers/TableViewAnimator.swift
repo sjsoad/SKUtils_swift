544170cdb0e2780d2e60c76a85fe909ca0230f1f
Sergey <sjsoad666@yandex.ru>
2018-04-06 10:40:21 +0300

    no message

--- Utils/Utils/Helpers/TableViewAnimator.swift
+++ Utils/Utils/Helpers/TableViewAnimator.swift
@@ -13,28 +13,36 @@ class TableViewAnimator: NSObject {
     // MARK: - Rows animation -
     
     class func deleteRows(in tableView: UITableView, at indexPathes: [IndexPath], animation: UITableViewRowAnimation) {
-        tableView.beginUpdates()
-        tableView.deleteRows(at: indexPathes, with: animation)
-        tableView.endUpdates()
+        execute(block: {
+            tableView.deleteRows(at: indexPathes, with: animation)
+        }, duringTableViewUpdate: tableView)
     }
     
     class func insertRows(in tableView: UITableView, at indexPathes: [IndexPath], animation: UITableViewRowAnimation) {
-        tableView.beginUpdates()
-        tableView.insertRows(at: indexPathes, with: animation)
-        tableView.endUpdates()
+        execute(block: {
+            tableView.insertRows(at: indexPathes, with: animation)
+        }, duringTableViewUpdate: tableView)
     }
     
     // MARK: - Sections animation -
     
     class func deleteSections(in tableView: UITableView, at indexes: [Int], animation: UITableViewRowAnimation) {
-        tableView.beginUpdates()
-        tableView.deleteSections(IndexSet(indexes), with: animation)
-        tableView.endUpdates()
+        execute(block: {
+            tableView.deleteSections(IndexSet(indexes), with: animation)
+        }, duringTableViewUpdate: tableView)
     }
     
     class func insertSections(in tableView: UITableView, at indexes: [Int], animation: UITableViewRowAnimation) {
+        execute(block: {
+            tableView.insertSections(IndexSet(indexes), with: animation)
+        }, duringTableViewUpdate: tableView)
+    }
+    
+    // MARK: - Private -
+    
+    private class func execute(block: () -> Void, duringTableViewUpdate tableView: UITableView) {
         tableView.beginUpdates()
-        tableView.insertSections(IndexSet(indexes), with: animation)
+        block()
         tableView.endUpdates()
     }
     
